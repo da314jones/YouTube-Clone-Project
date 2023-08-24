eec7980b4dde8a9f6b5599a1e0df68a0a0953497
Nicole Slater <nicoleslater@pursuit.org>
2023-08-23 20:15:49 -0400

    Fetch.JS

--- Youtube-Clone-Project/fetch.js
+++ Youtube-Clone-Project/fetch.js
@@ -1,2 +1,57 @@
 const URL = import.meta.env.VITE_BASE_API_URL
-fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${URL}`);
+const URL2 = import.meta.env.VITE_BASE_API_URL
+fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${URL2}`);
+
+// Create
+export function createVideos(videos) {
+  // we need to specify what type of request we're making in fetch
+  const options = {
+    method: "POST",
+    // body is a JSON version of our show object we got from state
+    body: JSON.stringify(videos),
+    // tell the server that this is JSON
+    headers: {"Content-Type": "application/json"}
+  }
+  return fetch(`${URL}/videos`, options).then(res => res.json())
+}
+
+// Delete
+export function destroyShow(id) {
+  // options object
+
+  const options = { method: "DELETE" }
+  return fetch(`${URL}/shows/${id}`, options)
+}
+
+// Index/Get all
+export function getAllVideos() {
+  // this function hjits our Server at the /shows endpoint
+  // and returns a PROMISE with some JSON inside
+  return fetch(`${URL}/videos`).then(res => res.json())
+}
+
+// Show/Get one
+export function getOneShow(id) {
+  // gets data from the shows/:id route
+  // beause we are getting one resource we need to tell the serve
+  // WHICH ONE WE WANT
+  return fetch(`${URL}/shows/${id}`).then(res => res.json())
+}
+
+// Update
+export function updateShow(id, show) {
+  const options = {
+    method: "PUT",
+    // body is a JSON version of our show object we got from state
+    body: JSON.stringify(show),
+    // tell the server that this is JSON
+    headers: {"Content-Type": "application/json"}
+  }
+  return fetch(`${URL}/shows/${id}`, options).then(res => res.json())
+}
+
+// Movies
+
+export function getAllMovies() {
+  return;
+}
--- Youtube-Clone-Project/src/App.jsx
+++ Youtube-Clone-Project/src/App.jsx
@@ -2,8 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { getAllVideos, createVideos } from '../fetch';
 
 function App() {
+  console.log(getAllVideos)
+  console.log(createVideos)
+
   const [count, setCount] = useState(0)
 
   return (
