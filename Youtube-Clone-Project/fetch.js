a0a886337d38301da131767b148673429b537aeb
Nicole Slater <nicoleslater@pursuit.org>
2023-08-25 00:14:32 -0400

    Adjusting fetch URL

--- Youtube-Clone-Project/fetch.js
+++ Youtube-Clone-Project/fetch.js
@@ -6,7 +6,7 @@ const URL = import.meta.env.VITE_BASE_API_KEY
 
 export function getAllVideos(){
 
-    return fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${URL}`)
+    return fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${URL}&channelId=${channelId}&part=snippet,id&order=date&maxResult=20`)
 
     .then(res => res.json())
 }
