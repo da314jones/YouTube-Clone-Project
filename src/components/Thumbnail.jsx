fbd8fe22437479378537889fda694ffb10ef3a9b
da314jones <dwaynejones@pursuit.org>
2023-08-25 23:23:26 -0400

    Resolved merge conflicts and merged upstream/nicole into main

---
+++ src/components/Thumbnail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Thumbnail(video) {
+
+    return (
+        <div className='thumcnail'>
+            <img src="" alt="" />
+            <p>{videotitle}</p>
+        </div>
+    )
+}
