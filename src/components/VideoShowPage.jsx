97aca8cd4afa8cc353465aa7f4651c0a2b4f7b3d
da314jones <dwaynejones@pursuit.org>
2023-08-27 15:34:51 -0400

    merge resolution

---
+++ src/components/VideoShowPage.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+
+export default function VideoShowPage() {
+    const { videId } = useParams();
+    const [video, setvideo] = useState(null);
+    
+    useEffect(() => {
+
+    })
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
