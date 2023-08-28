83a1c2f65c68e8d1430847c45c3cab6872ae3a76
da314jones <dwaynejones@pursuit.org>
2023-08-27 22:05:15 -0400

    most recent changes

--- src/components/IframePlyer.jsx
+++ src/components/IframePlyer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function IframePlayer({videoSrc}) {
   return (
     <iframe id="player" type="text/html" width="640" height="390"
-  src="http://www.youtube.com/embed/hCMdVywETkU?enablejsapi=1&origin=http://example.com"
+  src=`http://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=http://example.com`
   frameborder="0"></iframe>
   )
 }
