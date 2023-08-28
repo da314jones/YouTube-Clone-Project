def501fa1bc25fe127de60c42bcd29e183d7d448
Nicole Slater <nicoleslater@pursuit.org>
2023-08-27 23:55:40 -0400

    Debugging

--- src/components/IframePlayer.jsx
+++ src/components/IframePlayer.jsx
@@ -3,6 +3,6 @@ export default function IframePlayer({videoSrc}) {
 return (
 <iframe id="player" type="text/html" width="640" height="390"
  src="http://www.youtube.com/embed/hCMdVywETkU?enablejsapi=1&origin=http://example.com"
- frameborder="0"></iframe>
+ 
  )
 }
