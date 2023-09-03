713e4e5906beb00f9dfe91438b1c817fde9f59ff
da314jones <dwaynejones@pursuit.org>
2023-09-03 02:33:30 -0400

    adding team info set up

--- src/components/TeamInfo.jsx
+++ src/components/TeamInfo.jsx
@@ -1,42 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import Djones from "./Djones";
+import "./TeamInfo.css";
 import { Card } from "react-bootstrap";
 
 export default function TeamInfo() {
+  const [modalShow, setModalShow] = useState(false);
+
   return (
-    <div className="team-info-container flex justify-between space-x-6">
-      <Card className="card mx-2 p-4">Nicole Slater</Card> <Card border="success" style={{ width: '18rem' }}>
-        <Card.Header>Header</Card.Header>
-        <Card.Body>
-          <Card.Title>Success Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <br />
-      <Card className="card mx-2 p-4">Dwayne Jones</Card> <Card border="success" style={{ width: '18rem' }}>
-        <Card.Header>Header</Card.Header>
-        <Card.Body>
-          <Card.Title>Success Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <br />
-      <Card className="card mx-2 p-4">Jefferson Chua</Card> <Card border="success" style={{ width: '18rem' }}>
-        <Card.Header>Header</Card.Header>
-        <Card.Body>
-          <Card.Title>Success Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <br />
-    </div>
+    <>
+      <div className="team-info-container flex justify-between space-x-6">
+        <Card className="card flex-auto mx-2 p-4 djones-card style={{ width: '18rem' }}">
+          <Card.Body style={{ width: "18rem" }}>
+            <Card.Title>
+              <h1
+                className="text-3xl hover:bg-blue-500 hover:text-white hover:shadow-lg cursor-pointer"
+                onClick={() => setModalShow(true)}
+              >
+                Dwayne Jones
+              </h1>
+            </Card.Title>
+            <br />
+            <Card.Text>
+              You have been looking at one of our first major team projects.
+              When you are done here please take a look at our personal sites
+              and look at some more of our work. <br />
+              Thanks for stopping by.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <Djones show={modalShow} onHide={() => setModalShow} />
+    </>
   );
 }
